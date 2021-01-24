962dbe59b5e9fa68434b72f0cb65d85015f0c1e0
misganaw meseret <it.misganaw.meseret1@gmail.com>
2021-01-24 23:33:31 +0300

    finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,51 @@
-function deposite(){}
-function withdraw(){}
-function transfer(){}
-(function(){
-})();
+var balance = 1000;
+
+menu();
+
+function withdraw(
+){ var withdrawal = parseInt(prompt("enter withdrawal amount"))
+if(parseInt(balance) < withdrawal){
+    alert('insufficient funds')
+    getbalance()
+}
+else{
+    balance -= withdrawal;
+    getbalance()}
+}
+
+
+function deposite(){ 
+    var depositeamount = parseInt(prompt("enter deposite amount"))
+    balance += depositeamount;
+    getbalance();
+}
+
+function transfer(){
+    var account= parseInt(prompt("enter account number"))
+    var amount = parseInt(prompt("enter amount"))
+    balance -= amount
+    alert('transferred succesfully')
+    getbalance();
+}
+function getbalance() {
+    alert('Your current balance is: '+balance);
+    menu();
+}
+function menu(){
+    
+    var choice = prompt("welcome to M BANK- 1.Deposite 2.withdraw 3.transfer 4.to checkbalance 5 to exit")
+    if (choice == 1){
+        deposite()
+    }  
+    if (choice == 2){
+        withdraw()
+    }
+    if(choice == 3){
+        transfer()
+    }
+    if(choice==4){
+        getbalance()
+    }
+    else{ balance()}
+
+}
